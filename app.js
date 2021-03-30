b3c1872337d043ae04e274a1b43cbc80b1635a03
mforkan9 <mforka9@gmail.com>
2021-03-30 19:15:10 +0600

    update

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const foodDisplay = foods =>{
         const div =document.createElement('div')
         div.className = 'food'
         div.innerHTML = `
-            <div onClick="foodDetails('${foodName.strCategory}')">
-                <img class='food-img' src='${foodName.strCategoryThumb}'>
-                <h3>${foodName.strCategory}</h3>
-            </div>
-        `
+             <div onClick="foodDetails('${foodName.strCategory}')">
+             <img class='food-img' src='${foodName.strCategoryThumb}'>
+                 <h3>${foodName.strCategory}</h3>
+         </div>`
+        
         foodSection.appendChild(div)
    } 
    const foodSections = document.getElementById('foods')
@@ -57,7 +57,12 @@ const foodDisplay = foods =>{
                 div.innerHTML = `
                     <div>
                         <img class="food-img" src="${kanaItem.strMealThumb}">
-                        <h3>${kanaItem.strMeal}</h3>
+                        
+                        </br>
+                        <div>
+                        <h4>${kanaItem.strMeal}</h4>
+                        <button class="btn btn-success"  onClick="kanaDetails('${kanaItem.idMeal} && ${itemSection}')">Details</button>
+                        </div>
                     </div>
                 `
                 searchDetails.appendChild(div)
@@ -78,7 +83,7 @@ const foodDisplay = foods =>{
                 const div = document.createElement('div')
                 div.className = 'food'
                 div.innerHTML = `
-                    <div>
+                    <div onclick="videoSearch('${kanaItem.strMeal}')">
                         <img class="food-img" src="${kanaItem.strMealThumb}">
                         <h3>${kanaItem.strMeal}</h3>
                     </div>
@@ -91,4 +96,40 @@ const foodDisplay = foods =>{
         btn.addEventListener('click', function (){
             //const foodSe = document.getElementById('foods');
             foodSection.style.display = 'none'
+        })
+
+        const kanaDetails = (clickItem) =>{
+            fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${clickItem}`)
+            .then(res => res.json())
+            .then(data => displayKabar(data.meals))
+        }
+
+        const displayKabar = kabar =>{
+            const details = document.getElementById('kanaDetails')
+            kabar.map(kabarItem =>{
+                const div = document.createElement('div')
+                div.innerHTML = `
+                <div>
+                    <img class='detailImg' src="${kabarItem.strMealThumb}">
+                    <div class='showCenter'>
+                       <h1>${kabarItem.strMeal}</h1>
+                       <ul>
+                            <li>${kabarItem.strMeasure1} <span>${kabarItem.strIngredient1}</span></li>
+                            <li>${kabarItem.strMeasure2} <span>${kabarItem.strIngredient2}</span></li>
+                            <li>${kabarItem.strMeasure3} <span>${kabarItem.strIngredient3}</span></li>
+                            <li>${kabarItem.strMeasure4} <span>${kabarItem.strIngredient4}</span></li>
+                            <li>${kabarItem.strMeasure5} <span>${kabarItem.strIngredient5}</span></li>
+                            <li>${kabarItem.strMeasure6} <span>${kabarItem.strIngredient6}</span></li>
+                            <li>${kabarItem.strMeasure7} <span>${kabarItem.strIngredient7}</span></li>
+                            <li>${kabarItem.strMeasure8} <span>${kabarItem.strIngredient8}</span></li>
+                       </ul>
+                    </div>
+                </div>
+                `
+                details.appendChild(div)
+            })
+        }
+         const itemSection = document.getElementById('food-details')
+        itemSection.addEventListener('click',() => {
+            itemSection.style.display = 'none'
         })
